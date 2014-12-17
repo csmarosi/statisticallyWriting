ba9d4e38cc359df8b39a1eedbf6e04bf5c608fd1
Csaba Marosi <cs.marosi@gmail.com>
2014-12-17 22:26:51 +0000

    using argparse
    fixes #1

--- textGenerator.py
+++ textGenerator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
+import argparse
 import codecs
-from sys import stderr
+from sys import stdout
 import textGeneratorBackend
 
 
@@ -16,25 +17,37 @@ def createWordlist(fileName):
 
 
 def writeList(fileName, finalList):
-    out = codecs.open(fileName, 'w', 'utf-8')
+    out = stdout
+    if fileName != '_SDTOUT_':
+        out = codecs.open(fileName, 'w', 'utf-8')
     for w in finalList:
         out.write(w)
     out.write('\n')
 
 
 def main():
-    wo = createWordlist('text/edesAnna')
+    parser = argparse.ArgumentParser(description='In this paper, we...')
+    parser.add_argument("-c", "--character", action="store_true",
+                        help='Process text on character level')
+    parser.add_argument("-p", "--penalty", type=int, default=10)
+    parser.add_argument("-n", "--gramN", type=int, default=3)
+    parser.add_argument("-w", "--wordNum", type=int, default=60)
+    parser.add_argument("inputFile")
+    parser.add_argument("-o", "--outFile", type=str, default='_SDTOUT_',
+                        help='stdout is used if not given specified')
+    args = parser.parse_args()
+
+    wo = createWordlist(args.inputFile)
     w = []
-    character = True
-    if character:
+    if args.character:
         for wi in wo:
             for wj in wi:
                 w.append(wj)
     else:
         w = wo
     o = textGeneratorBackend.StatisticsTextGenerator(
-        penalty=10, gramN=4, wordNum=1940)
-    writeList('edesAnna.genText', o.generateText(w))
+        penalty=args.penalty, gramN=args.gramN, wordNum=args.wordNum)
+    writeList(args.outFile, o.generateText(w))
 
 
 if __name__ == "__main__":
